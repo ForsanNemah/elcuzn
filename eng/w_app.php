e68446397a84c003133aa4fa81ec845d1809741f
ForsanNemah <forsan20172017@gmail.com>
2023-08-08 03:48:49 +0300

    dd

--- eng/w_app.php
+++ eng/w_app.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<a href="https://wa.link/sxiq8y" class="float" target="_blank">
+<a href="https://wa.me/+17134598789" class="float" target="_blank">
 <i class="fa fa-whatsapp my-float"></i>
 </a>
 
