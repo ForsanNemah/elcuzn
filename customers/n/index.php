be4c9d7baf671ec42b5d7e9c7616b9776f0c72d0
ForsanNemah <forsan20172017@gmail.com>
2023-08-16 01:58:18 +0300

    Update index.php

--- customers/n/index.php
+++ customers/n/index.php
@@ -45,7 +45,30 @@ include 'info.php';
 
   
   
- 
+  </title>
+  
+    <style>
+        /* WhatsApp Button */
+
+        .whatsApp {
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            right: 10px;
+            background-color: #25d366;
+            color: #FFF;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+        }
+
+        .my-whatsApp {
+            margin-top: 9px;
+        }
+    </style>
 
 
 
